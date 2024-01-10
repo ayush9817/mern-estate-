2e8da1f114eb296b93897cf78c55708e56391090
ayush9817 <96259709+ayush9817@users.noreply.github.com>
2024-01-10 15:40:39 +0530

    axios

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx';
 import './index.css';
 import { persistor, store } from './redux/store.js';
 import {Provider} from 'react-redux';
+import axios from 'axios';
 import { PersistGate } from 'redux-persist/integration/react';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
@@ -11,4 +12,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
     </PersistGate>
   </Provider>,
+
 )
